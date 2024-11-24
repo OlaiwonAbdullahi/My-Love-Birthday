5ad5e5389b8a6274528e50ec02ee264427735317
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-24 19:43:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { CiHome } from "react-icons/ci";
 
 function App() {
   return (
-    <div className="bg-secondary text-text font-">
+    <div className="bg-secondary text-text font-Parkinsans">
       <div className="flex justify-between item-center">
         <div className=" flex item-center text-primary">
           <CiHome className="text-primary h-6 w-6" /> Home
