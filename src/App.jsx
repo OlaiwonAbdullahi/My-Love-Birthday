e26824367e993c9f34b4e9ed67104dff659cd500
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-24 19:56:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,15 @@ import { CgMenuRound } from "react-icons/cg";
 function App() {
   return (
     <div className="bg-secondary text-text font-Parkinsans min-h-screen px-4 py-2">
-      <div className="flex justify-between items-center pb-2">
-        
+      <div className="flex justify-between items-center pb-2">   
         <div className="flex items-center gap-2 text-primary">
-          <CiHome className="h-6 w-6" />
-          <span className="text-base font-medium">Home</span>
+          <CiHome className="h-7 w-7" />
         </div>
         <div className="text-sm text-text font-medium">
           13th May 2025
         </div>  
         <div>
-          <CgMenuRound className="h-6 w-6 text-primary" />
+          <CgMenuRound className="h-7 w-7 text-primary" />
         </div>
       </div>
       <div className="mt-4 text-center">
