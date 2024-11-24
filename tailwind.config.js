69f97e274791b82cdb35745f457ca37d48b20198
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-24 18:59:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Agdasima: ["Agdasima", "serif"],
+        Montserrat: ["Montserrat", "serif"],
+      },
+      colors: {
+        primary: "#0F1A1A",
+        primary1: "#021110",
+        primaryHover: "#022E2B",
+        secondary: "#07F0FF",
+        text: "#CDCDCD",
+      },
+    },
   },
   plugins: [],
-}
+};
