98479a8e158c2950eb08e919c322251ae309d29b
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-24 19:28:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { CiHome } from "react-icons/ci";
 
 function App() {
   return (
-    <div class name=" bg-secondary text-text">
-    <div className=" flex justify-between">
-<div><CiHome class name=" text-primary"/> Home</div>
-<div> 13th May 2025</div>
-<div> <Menu/ </div>
-</div>
+    <div className="bg-secondary text-text">
+      <div className="flex justify-between">
+        <div>
+          <CiHome className="text-primary" /> Home
+        </div>
+        <div>13th May 2025</div>
+        <div><Menu /></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
