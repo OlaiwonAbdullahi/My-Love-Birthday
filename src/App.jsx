ca40b4b4b74fe33ca771e23eea469fca01b0ca5a
Abdullahi Olaiwon <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-11-24 20:03:17 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { CiHome } from "react-icons/ci";
 import { CgMenuRound } from "react-icons/cg";
 
 function App() {
-  const today = new Date().toLocaleDateString("en-GB", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  });
 
   return (
     <div className="bg-secondary text-text font-Parkinsans min-h-screen px-4 py-2">
@@ -14,9 +9,9 @@ function App() {
         <div className="flex items-center gap-2 text-primary">
           <CiHome className="h-7 w-7" aria-label="Home icon" />
         </div>
-        <time dateTime={new Date().toISOString()} className="text-sm text-text font-medium">
-          {today}
-        </time>  
+        <div className="text-sm text-text font-medium">
+          13 May 2025
+        </div>  
         <div>
           <CgMenuRound className="h-7 w-7 text-primary" aria-label="Menu icon" />
         </div>
